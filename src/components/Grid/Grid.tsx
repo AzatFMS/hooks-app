e1513efa40933d601729d605e1c3d764ecc0930b
azat.gumerov <azat.gumerov@rtmis.ru>
2020-08-28 14:34:59 +0300

    Grid on hooks

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-
-import 'bootstrap/dist/css/bootstrap.css';
+import React, {ReactElement, useCallback, useEffect, useState} from 'react';
 
 export interface GridItemData {
   [key: string]: string | number;
@@ -26,147 +24,38 @@ interface GridProps<TItem extends GridItemData> {
   filter: GridFilter<TItem>;
 }
 
-interface GridState {
-  sortColumnId: ColumnId | null;
-  sortDirection: SortDirection;
-  viewData: GridItemData[];
-  filterText?: string;
-}
+function Grid<TItem extends GridItemData> ({
+  columns,
+  data,
+  filter,
+ }: GridProps<TItem>): ReactElement {
 
-class Grid<TItem extends GridItemData> extends Component<GridProps<TItem>, GridState> {
+  const [filterText, setFilterText] = useState<string>();
+  const handlerInputFilterText = useCallback((e: React.ChangeEvent<HTMLInputElement>): void => {
+    setFilterText(e.target.value);
+  }, []);
 
-  state: GridState = {
-    sortColumnId: null,
-    sortDirection: SortDirection.ASC,
-    viewData: [],
-  }
+  const [sortColumnId, setSortColumnId] = useState<ColumnId | null>(null);
+  const [sortDirection, setSortDirection] = useState<SortDirection>(SortDirection.ASC);
 
-  render() {
-
-    const {
-      columns,
-    } = this.props;
-
-    const {
-      sortColumnId,
-      sortDirection,
-      viewData,
-      filterText,
-    } = this.state;
-
-    return (
-      <>
-        <div className="input-group my-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Поиск"
-            value={filterText}
-            onChange={this.handlerInputFilterText}
-          />
-        </div>
-        <table className="table table-striped">
-          <thead>
-          <tr>
-            {
-              columns.map(({ id, title }) => (
-                  <th
-                    key={id}
-                    className="cursor-pointer"
-                    onClick={() => {
-                      this.setOrder(id);
-                    }}
-                  >
-                      {title}
-                      {
-                        sortColumnId === id &&
-                        <span className="text-primary">
-                          {
-                            sortDirection
-                              ? ' ↑'
-                              : ' ↓'
-                          }
-                        </span>
-                      }
-                  </th>
-              ))
-            }
-          </tr>
-          </thead>
-          <tbody>
-          {
-            viewData.map(item => (
-                <tr>
-                  {
-                    columns.map(({ id: columnId }) => (
-                        <td
-                          key={columnId}
-                        >
-                          { item[columnId] }
-                        </td>
-                    ))
-                  }
-                </tr>
-            ))
-          }
-          </tbody>
-        </table>
-      </>
-    );
-  }
-
-  componentDidMount() {
-    this.generateViewData();
-  }
-
-  componentDidUpdate(prevProps: Readonly<GridProps<TItem>>) {
-    if (prevProps.data !== this.props.data) {
-      this.generateViewData();
-    }
-  }
-
-  private handlerInputFilterText = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({
-      filterText: e.target.value
-    }, this.generateViewData);
-  }
-
-  private setOrder = (sortColumnId: ColumnId | null): void => {
-
-    this.setState({
-      sortColumnId,
-      sortDirection: this.getSortDirectionByNewSortColumnId(sortColumnId),
-    }, this.generateViewData)
-  }
-
-  private getSortDirectionByNewSortColumnId = (sortColumnId: ColumnId | null): SortDirection => {
-    const {
-      sortColumnId: prevSortColumnId,
-      sortDirection: prevSortDirection,
-    } = this.state;
-
-    if (sortColumnId === prevSortColumnId) {
-      return prevSortDirection === SortDirection.ASC
+  const setOrder = useCallback((newSortColumnId: ColumnId | null): void => {
+    const getSortDirection = (): SortDirection => {
+      if (sortColumnId === newSortColumnId) {
+        return sortDirection === SortDirection.ASC
           ? SortDirection.DESC
           : SortDirection.ASC;
+      }
+
+      return  SortDirection.ASC;
     }
+    setSortDirection(getSortDirection());
+    setSortColumnId(newSortColumnId);
+  }, [sortColumnId, sortDirection]);
 
-    return  SortDirection.ASC;
-  }
+  const [viewData, setViewData] = useState<TItem[]>([]);
 
-  private generateViewData = (): void => {
-    const {
-      data,
-      filter,
-    } = this.props;
-
-    const {
-      sortColumnId,
-      sortDirection,
-      filterText,
-    } = this.state;
-
-    const viewData = data
+  useEffect(() => {
+    setViewData(data
       .filter(item => {
         if (!filterText) {
           return true;
@@ -181,12 +70,75 @@ class Grid<TItem extends GridItemData> extends Component<GridProps<TItem>, GridS
           return a[sortColumnId] > b[sortColumnId] ? 1 : -1;
         }
         return a[sortColumnId] > b[sortColumnId] ? -1 : 1;
-      });
+      })
+    );
+  }, [
+    data,
+    filter,
+    filterText,
+    sortDirection,
+    sortColumnId,
+  ]);
 
-    this.setState({
-      viewData,
-    })
-  }
+  return (
+    <>
+      <div className="input-group my-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Поиск"
+          value={filterText}
+          onChange={handlerInputFilterText}
+        />
+      </div>
+      <table className="table table-striped">
+        <thead>
+        <tr>
+          {
+            columns.map(({ id, title }) => (
+              <th
+                key={id}
+                className="cursor-pointer"
+                onClick={() => {
+                  setOrder(id);
+                }}
+              >
+                {title}
+                {
+                  sortColumnId === id &&
+                  <span className="text-primary">
+                          {
+                            sortDirection
+                              ? ' ↑'
+                              : ' ↓'
+                          }
+                        </span>
+                }
+              </th>
+            ))
+          }
+        </tr>
+        </thead>
+        <tbody>
+        {
+          viewData.map(item => (
+            <tr>
+              {
+                columns.map(({ id: columnId }) => (
+                  <td
+                    key={columnId}
+                  >
+                    { item[columnId] }
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Grid;
